6c50bbc6d4f5222bb5dc9f72569eff44d6840814
Usmaan Ahmed <158786456+usmaan04@users.noreply.github.com>
2025-02-04 15:40:32 +0000

    Update task4.hs

--- lab2-functionalprimer/haskell/task4.hs
+++ lab2-functionalprimer/haskell/task4.hs
@@ -1,14 +1,18 @@
+--Modifications are highlighted
 ask :: String -> IO ()
 ask prompt =
-  do
-  putStrLn prompt
-  line <- getLine
-  if line == ""
-    then ask prompt
-    else putStrLn ("you said: " ++ reverse line)
+    do
+    putStrLn prompt
+    line <- getLine
+    if line == ""
+        then ask prompt
+    else if (line == "quit") then putStrLn("quitting..") --condition added for quitting
+    else do
+        putStrLn ("you said: " ++ reverse line)
+        ask prompt --needed, otherwise programme will stop after printing reverse line
 
 main :: IO ()
 main =
-  do
-  let prompt = "please say something"
-  ask prompt
+    do
+    let prompt = "please say something"
+    ask prompt
